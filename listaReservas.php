fa869568dcbfda7d08fdf4c62615c9a29af210f0
JosePaulo95 <fernandes.josepaulo95@gmail.com>
2018-07-16 11:38:11 -0300

    lendo json estat e marcando tabs q tem algo

--- listaReservas.php
+++ listaReservas.php
@@ -18,16 +18,58 @@ class ListaReservas {
          }'
     );*/
     private $monthLabels = array("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez");
-
+    private $myJSON;
 
     public function show() {
-        $myJSON = json_decode('
-            {"pessoa1":{"name":"John","age":30,"city":"New York"}}
+        $this->myJSON = json_decode('
+            {"Jan2019":
+                {
+                    "2019-01-12":{
+                        "oferta1":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        },
+                        "oferta2":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        }
+                    },
+                    "2019-01-13":{
+                        "oferta1":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        },
+                        "oferta2":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        }
+                    }
+                },
+            "Set2019":
+                {
+                    "2019-07-12":{
+                        "oferta1":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        },
+                        "oferta2":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        }
+                    },
+                    "2019-07-13":{
+                        "oferta1":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        },
+                        "oferta2":{
+                            "cliente1": {"nome":"Joao", "vagas":"2"},
+                            "cliente2": {"nome":"Maria", "vagas":"6152"}
+                        }
+                    }
+                }
+            }
         ');
-
-        echo $myJSON->pessoa1->name;
-
-
 
         $calendars = '<div style="cursor: pointer;">';
 
@@ -37,14 +79,61 @@ class ListaReservas {
         $calendars .= $this->geraTabs($m, $y, 18);
 
         $calendars .= '</div>';
+
+        $calendars .= $this->geraContentTabs();
         return $calendars;
     }
+    public function geraContentTabs(){
+        $content = '<div style="cursor: pointer;">';
 
+        foreach($this->myJSON as $keyMes => $mes){
+            $content.= '<div id="'.$keyMes.'" class="tabcontent">';
+                foreach($this->myJSON->$keyMes as $keyDia => $dia){
+                    $content.='
+                        <div id="dt'.$keyDia.'" class="data">
+                            <p>'.$keyDia.'</p>
+                        </div>
+                    ';
+                    foreach($this->myJSON->$keyMes->$keyDia as $keyOferta => $oferta){
+                        $content.='
+                            <div id="'.$keyOferta.'" class="dt'.$keyDia.' oferta">
+                                <p>'.$keyOferta.'</p>
+                                <button style="display: inline-block;float: right;margin: 15px;" type="button">Confirmar viagem</button>
+                            </div>
+                        ';
+                        foreach($this->myJSON->$keyMes->$keyDia->$keyOferta as $keyCliente => $cliente){
+                            $content.='
+                                <div class="cliente cdt'.$keyDia.' dt'.$keyDia.'-'.$keyOferta.'">
+                                    <p>'.$cliente->nome.'</p>
+                                </div>
+                            ';
+                        }
+                    }
+                }
+            $content.= '</div>';
+        }
+
+        $tabsParaVazias ='
+                    <div id="mes_nenhuma_reserva" class="tabcontentnull">
+                        <p>
+                            Companheiro, você ainda não tem reservas para este mês.
+                        </p>
+                    </div>
+                    <div id="todos_nenhuma_reserva" class="tabcontentnull">
+                        <p>
+                            No momento, não há nenhum pedido de reserva para suas ofertas.
+                        </p>
+                    </div>';
+        $content .= $tabsParaVazias;
+        $content.= '</div>';
+
+        return $content;
+    }
     public function geraTabs($mes_inicial, $ano_inicial, $qtde_meses){
         $tabs_labels = '<div class="tab">';
 
         $tem_algo = $this->checaQualquerReserva();
-        $tabs_labels .= '<button id="tab_todos" class="tablinks" onclick="showTodos(event)"> <p class="tab_ano"><br></p><p class="tab_mes">Todos</p> </button>';
+        $tabs_labels .= '<button id="tab_todos" class="tablinks '.$tem_algo.'" onclick="showTodos(event)"> <p class="tab_ano"><br></p><p class="tab_mes">Todos</p> </button>';
 
         $mes_inicial--;
         $label_ano = $ano_inicial;
@@ -68,11 +157,13 @@ class ListaReservas {
         return $tabs_labels;
     }
     private function checaMesTemAlgo($index_mes, $label_ano){
-        return ($index_mes%6 == 0)?'tem_algo':'';
+        $index = $this->monthLabels[$index_mes];
+        $index .= "20".$label_ano;;
+        return array_key_exists($index, $this->myJSON)?"tem_algo":'';
     }
     private function checaQualquerReserva(){
-        return true;//TODO: checar o tamanho de $this->data
+        //echo count(($this->myJSON));
+        return (count($this->myJSON)>0)?"tem_algo":'';
     }
 }
-
 ?>
